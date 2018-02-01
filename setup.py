c4a0d781a7ff4ec303a3e85b8c2389c52ca349da
Bastian <basti@bastibe.de>
2018-02-01 15:58:51 +0100

    updates version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='runforrest',
-    version='0.1.0',
+    version='0.2.0',
     description='Batch and run your code in parallel. Simply.',
     author='Bastian Bechtold',
     author_email='basti@bastibe.de',
